302046fc4879b331bfdf502e6ac70f90c5f30b25
KatarinaLackner <Katarina.Lackner@gmail.com>
2016-10-31 00:15:22 -0500

    Functionality before history

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,9 +1,20 @@
 class GuessesController < ApplicationController
+
   def index
-    render("guesses/index.html.erb")
-  end
+
+    @first = params["first_number"]
+    @second = params["second_number"]
+    @third = params["third_number"]
+
+render("guesses/index.html.erb")
+end
 
   def answer
+
+    #params = {"rule" => "Hi there"}
+
+    @user_answer = params["rule"]
+
     render("guesses/answers.html.erb")
   end
 end
